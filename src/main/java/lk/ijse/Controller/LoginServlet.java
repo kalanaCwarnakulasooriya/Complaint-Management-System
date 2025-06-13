d9df8b5ee3b6d35ed7ecb7835438925835e1d7d0
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-06-14 00:20:56 +0530

    Login Servlet

---
+++ src/main/java/lk/ijse/Controller/LoginServlet.java
@@ -0,0 +1,42 @@
+package lk.ijse.Controller;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lk.ijse.Dao.UserDAO;
+import lk.ijse.Model.User;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        ServletContext context = req.getServletContext();
+        BasicDataSource dataSource = (BasicDataSource) context.getAttribute("dbcpDataSource");
+        UserDAO userDAO = new UserDAO(dataSource);
+
+        User user = userDAO.validateLogin(username, password);
+
+        if (user != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            if (user.getJobRole().equals("admin")) {
+                resp.sendRedirect("JSP/admin-dashboard.jsp");
+            } else {
+                resp.sendRedirect("JSP/employee-dashboard.jsp");
+            }
+        } else {
+            req.setAttribute("error", "Invalid username or password.");
+            req.getRequestDispatcher("JSP/login.jsp").forward(req, resp);
+        }
+    }
+}
